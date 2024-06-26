13a8b4c0cd5d997a28fc7c7de9606a7100ab133d
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-26 00:45:55 -0300

    improved: reformat code with black

--- api/controllers/balances_controller.py
+++ api/controllers/balances_controller.py
@@ -6,10 +6,12 @@ from fastapi.routing import APIRouter
 
 BALANCES_ROUTER = APIRouter()
 
-@BALANCES_ROUTER.post('/reset')
-async def reset():
-    return 'OK'
 
-@BALANCES_ROUTER.get('/balances')
+@BALANCES_ROUTER.post("/reset")
+async def reset():
+    return "OK"
+
+
+@BALANCES_ROUTER.get("/balances")
 async def balances(account_id: int | str = None) -> int:
     return 0
