3bf9edeec4046deec7a502c2233f370a6de0de7a
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-27 20:22:20 -0300

    feat: switch logic to account service

--- api/controllers/event_controller.py
+++ api/controllers/event_controller.py
@@ -2,9 +2,9 @@
 Router Module for Control Event path
 """
 
-from fastapi import Depends, Response, status
+from fastapi import Response, status, HTTPException
 from fastapi.routing import APIRouter
-from api.repositories.account_repository import get_account_repository
+from api.services.account_service import AccountServiceDependecy
 from api.forms.event_form import EventType
 
 EVENT_ROUTER = APIRouter()
@@ -12,8 +12,9 @@ EVENT_ROUTER = APIRouter()
 
 @EVENT_ROUTER.post("/event", status_code=status.HTTP_201_CREATED)
 async def event(
-    event_input: EventType, account_repository=Depends(get_account_repository)
+    event_input: EventType, account_service_manager: AccountServiceDependecy
 ):
-    account_repository.find_account("foo")
-
-    return Response("0", status.HTTP_404_NOT_FOUND)
+    try:
+        return event_input.event_action(account_service_manager)
+    except HTTPException as error:
+        return Response('0', error.status_code)
--- api/forms/event_form.py
+++ api/forms/event_form.py
@@ -4,26 +4,57 @@ Module for form types
 
 from typing import Literal, Union, Annotated
 from pydantic import BaseModel, Field
+from fastapi import HTTPException
+
+from api.services.account_service import AccountService
 
 
 class AbstractEventInput(BaseModel):
     amount: int = Field(ge=0)
+
+    def event_action(self, account_service: AccountService) -> dict:
+        raise NotImplementedError()
 
 
 class DepositEventInput(AbstractEventInput):
     type: Literal["deposit"]
     destination: str = Field(min_length=1)
 
+    def event_action(self, account_service: AccountService) -> dict:
+        account_state = account_service.increment_account(
+            self.destination, self.amount
+        )
+        return {"destination": account_state}
+
 
 class WithdrawEventInput(AbstractEventInput):
     type: Literal["withdraw"]
     origin: str = Field(min_length=1)
+
+    def event_action(self, account_service: AccountService) -> dict:
+        account_state = account_service.decrement_account(
+            self.origin, self.amount
+        )
+        return {"origin": account_state}
 
 
 class TranferEventInput(AbstractEventInput):
     type: Literal["transfer"]
     origin: str = Field(min_length=1)
     destination: str = Field(min_length=1)
+
+    def event_action(self, account_service: AccountService) -> dict:
+        origin_account_state = account_service.decrement_account(
+            self.origin, self.amount
+        )
+        destin_account_state = account_service.increment_account(
+            self.destination, self.amount
+        )
+
+        return {
+            "origin": origin_account_state,
+            "destination": destin_account_state,
+        }
 
 
 EventType = Annotated[
