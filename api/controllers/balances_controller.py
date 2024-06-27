7531e59819984255fd10411994951e49c2c85837
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-26 22:27:59 -0300

    improved: add repository to controller

--- api/controllers/balances_controller.py
+++ api/controllers/balances_controller.py
@@ -2,16 +2,22 @@
 Controller setting for not used responses
 """
 
+from fastapi import Depends
 from fastapi.routing import APIRouter
+from api.repositories.account_repository import get_account_repository
 
 BALANCES_ROUTER = APIRouter()
 
 
 @BALANCES_ROUTER.post("/reset")
-async def reset():
+async def reset(account_repository=Depends(get_account_repository)):
+    account_repository.reset_accounts()
     return "OK"
 
 
 @BALANCES_ROUTER.get("/balances")
-async def balances(account_id: int | str = None) -> int:
+async def balances(
+    account_id: str = None, account_repository=Depends(get_account_repository)
+) -> int:
+    account_repository.find_account(account_id)
     return 0
