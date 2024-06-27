f94cf4ecf14ce889e68314306e5ebea0b8a65eae
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-26 22:26:45 -0300

    feat: account init repository

---
+++ api/repositories/account_repository.py
@@ -0,0 +1,43 @@
+"""
+Repository of accounts
+"""
+
+from typing import Optional, Self
+from api.models.account import Account
+
+
+class AccountRepository:
+    """
+    Manage account api storage
+    """
+
+    __instance = None
+
+    def __init__(self):
+        self.__accounts: dict[str, Account] = {}
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def find_account(self, account_id: str) -> Optional[Account]:
+        """Get one account by id"""
+        return self.__accounts.get(account_id)
+
+    def save_account(self, account: Account) -> Self:
+        """Save/Update account"""
+        self.__accounts[account.account_id] = account
+        return self
+
+    def reset_accounts(self) -> Self:
+        """Clean all accounts"""
+
+        self.__accounts = {}
+        return self
+
+
+def get_account_repository() -> AccountRepository:
+    return AccountRepository.get_instance()
