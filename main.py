956cf57f6f4613f2cf6e5b75c7b7ea680853491f
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-06-27 21:53:43 -0300

    improved: add event router to main app

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ Api entry point.
 
 from fastapi import FastAPI
 from api.controllers.balances_controller import BALANCES_ROUTER
+from api.controllers.event_controller import EVENT_ROUTER
 
 app = FastAPI()
 app.include_router(BALANCES_ROUTER)
+app.include_router(EVENT_ROUTER)
 
 if __name__ == "__main__":
     print("Usage: uvicorn main:app [add --reload dev code]")
